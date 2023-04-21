f5f7f80c69162f884250144a2bd7ee19438ceb73
chefsichter <51502336+chefsichter@users.noreply.github.com>
2023-04-21 10:51:56 +0200

    udpated StderrLogger

--- fbratelegram/extras/stderr_logger.py
+++ fbratelegram/extras/stderr_logger.py
@@ -1,17 +1,18 @@
+import datetime
 import logging
 import sys
-import time
 
 
 class StderrLogger:
     STDERR_LOGGER = 'stderr-console'
 
     def __init__(self, propagate=True):
+        self.modul = 'ERROR - stderr'
         self.logger = self._create_logger(propagate)
-        self.buf = []
         sys.stderr = self
+        self.buf = []
         self.delay_time = 60
-        self.last_log = time.monotonic() - self.delay_time
+        self.last_log = datetime.datetime.now() - datetime.timedelta(seconds=self.delay_time)
 
     def _create_logger(self, propagate):
         stream_handler = logging.StreamHandler()
@@ -24,13 +25,17 @@ class StderrLogger:
         return logger
 
     def write(self, buf):
-        now = time.monotonic()
-        if (now - self.last_log) > self.delay_time:
+        now = datetime.datetime.now()
+        if (now - self.last_log).total_seconds() > self.delay_time:
             self.last_log = now
-            self.logger.error(f"Error from stderr:")
+            str_logging_time_stamp = now.strftime('%Y-%m-%d %H:%M:%S,%f')[:-3]
+            self.buf.append(f"{str_logging_time_stamp} - {self.modul}:\n")
         if buf.endswith('\n'):
             self.buf.append(buf.removesuffix('\n'))
-            self.logger.error(''.join(self.buf))
+            built_buf = ''.join(self.buf)
+            if built_buf == "" or built_buf.isspace():
+                built_buf = "_" * 10 + "empty_line" + "_" * 10
+            self.logger.error(built_buf)
             self.buf = []
         else:
             self.buf.append(buf)
--- fbratelegram/tests/test_without_testcase.py
+++ fbratelegram/tests/test_without_testcase.py
@@ -1,21 +1,22 @@
 import sys
 import threading
 import time
-from client import Client
+
+from fbratelegram.client import Client
 from test_keys import bot_token, chat_id
 
-client = Client(bot_token=bot_token, chat_id=chat_id, log_stderr=True)
-client.start_loop()
+client = Client(bot_token=bot_token, log_stderr=True)
+client.start_loop(blocking=False)
 sys.stderr.write("An error was raised")
 time.sleep(1)
 
 
 def err_func():
     time.sleep(1)
-    5/0
+    # 5/0
     raise Exception("Stderr ?")
 
 
 threading.Thread(target=err_func).start()
-time.sleep(2)
+time.sleep(20)
 client.stop_loop()
